68d0499fc13c9987276a53289e597668c7b5a2fe
alisson-moura <alisson.mo.moura@outlook.com.br>
2024-11-05 14:57:00 -0300

    feat: criação de conta

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
+import { Toaster } from "@/components/ui/toaster"
 import { router } from "./router";
 
 const queryClient = new QueryClient();
@@ -11,6 +12,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <Toaster />
     </QueryClientProvider>
   </StrictMode>
 );
