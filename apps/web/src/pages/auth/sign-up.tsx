96f07486d03371c5ab58c7547f11cb1955999101
alisson-moura <alisson.mo.moura@outlook.com.br>
2024-11-05 13:28:37 -0300

    feat: login and sign-up page

--- apps/web/src/pages/auth/sign-up.tsx
+++ apps/web/src/pages/auth/sign-up.tsx
@@ -5,17 +5,17 @@ import {
   CardFooter,
   CardHeader,
 } from "@/components/ui/card";
-import { FaListCheck } from "react-icons/fa6";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { FaListCheck } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 
-export function LoginPage() {
+export function SignUpPage() {
   return (
     <div className="flex min-h-screen flex-col lg:flex-row justify-center">
       <div className="hidden lg:block lg:w-1/2">
         <img
-          src="background-01.svg"
+          src="background-02.svg"
           alt="Login image"
           width={800}
           height={800}
@@ -30,35 +30,38 @@ export function LoginPage() {
             </div>
             <h2 className="text-3xl font-bold">Organiza</h2>
             <p className="text-sm text-muted-foreground">
-              Digite seu e-mail e senha para realizar o login
+              Cadastre-se na nossa plataforma para organizar seus grupos e listas
             </p>
           </CardHeader>
           <CardContent className="space-y-4">
+          <div className="space-y-2">
+              <Label htmlFor="name">Nome</Label>
+              <Input
+                id="name"
+                placeholder="ex: João da Silva"
+                required
+                type="text"
+              />
+            </div>
             <div className="space-y-2">
               <Label htmlFor="email">E-mail</Label>
               <Input
                 id="email"
-                placeholder="m@example.com"
+                placeholder="ex: acme@exemplo.com"
                 required
                 type="email"
               />
             </div>
             <div className="space-y-2">
               <Label htmlFor="password">Senha</Label>
-              <Input id="password" required type="password" />
+              <Input id="password" placeholder="ex: Random@1234" required type="password" />
             </div>
           </CardContent>
           <CardFooter className="flex flex-col space-y-4">
-            <Button className="w-full">Login</Button>
+            <Button className="w-full">Cadastrar</Button>
             <div className="text-sm text-center space-x-4">
-              <Link
-                className="text-primary hover:underline"
-                to="/forgot-password"
-              >
-                Esqueceu sua senha?
-              </Link>
-              <Link className="text-primary hover:underline" to="/sign-up">
-                Cadastrar-se
+              <Link className="text-primary hover:underline" to="/">
+                Voltar para o Login
               </Link>
             </div>
           </CardFooter>
